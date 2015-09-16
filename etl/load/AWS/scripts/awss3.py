d0cdf74ac23ebd25b1bf8054a4eafbda7b52f63f
丁拯 <01911@imgo.tv>
2015-09-16 09:16:25 +0800

    awss3 python codes

---
+++ etl/load/AWS/scripts/awss3.py
@@ -0,0 +1,48 @@
+import boto3
+
+class awss3:
+	default_bucket_name = 'cn-north-region-java'
+	def __init__(self):
+		self.s3 = boto3.resource('s3')
+		self.bucketsCached = False
+		self.bucketsCache = []
+	def listBuckets(self,useCache):
+		if useCache and self.bucketsCached:
+			return self.bucketsCache
+		else:
+			self.bucketsCached = True
+			self.bucketsCache = self.s3.buckets.all()
+			return self.bucketsCache
+	def listObject(self,bucketName):
+		buckets = self.listBuckets(True)
+		bucketNames = []
+		isCacheValue = self.bucketsCached
+		for b in buckets:
+			bucketNames.append(b.name)
+		if bucketName not in bucketNames and isCacheValue:
+			buckets = self.listBuckets(False)
+		
+		for bucket in buckets:
+			if(bucket.name == bucketName):
+				return bucket.objects.all()
+		
+		return None
+	def listObjectKey(self,bucketName):
+		objects = self.listObject(bucketName)
+		keys = []
+		if objects != None:
+			for obj in objects:
+				keys.append(obj.key)
+			return keys
+		else:
+			return None
+	def deleteObject(self,bucketName,key):
+		objects = self.listObject(bucketName)
+		if objects != None:
+			for obj in objects:
+				if obj.key == key:
+					obj.delete()
+	def upload(self,bucketName,key,filePath):
+		file = open(filePath,'rb')
+		result = self.s3.Bucket(bucketName).put_object(Key=key,Body=file)
+		return result
