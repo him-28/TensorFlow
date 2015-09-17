f713b3ca27399d8dadbe9f6463732302e94c4d24
jinyibin <jin_yb@imgo.tv>
2015-09-17 09:51:06 +0800

    订单报告api程序结构，具体方法内容还待实现（组织查询sql，组织响应报文待实现）

--- api/app.py
+++ api/app.py
@@ -11,13 +11,11 @@ import settings
 from base_handler import Handler
 import os
 DIR_PATH = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-
 import sys
 sys.path.append(DIR_PATH)
-sys.path.append(os.path.join(DIR_PATH,'./order'))
-from order_request_handler import OrderRequestHandler
-
-orderRequestHandler=OrderRequestHandler()
+sys.path.append(os.path.join(DIR_PATH,'./util'))
+from api_utils import get_property
+from db_utils import get_resultFromDb
 
 class HelloHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -26,51 +24,44 @@ class HelloHandler(tornado.web.RequestHandler):
 class OrderReportHandler(Handler,tornado.web.RequestHandler):
 	
 	def self_check_param(self,params):
-		return "not successs"
-	
+		#维度检查
+		#TODO {"dateRange":{"start":"2015-09-01","end":"yyyy-mm-dd"},"ids":["id1","id2"],"name":" ","priDim":{"date":"1"},"subDim":{"region":"1"},"page":"2","pageNum":"10"}
+		if params.has_key("priDim") and params["priDim"] and params.has_key("subDim") and params["subDim"]:
+			pri_dim=params["priDim"]
+			sub_dim=params["subDim"]
+			sub_dim_check=get_property("dim_check",pri_dim)
+			if not sub_dim_check or not sub_dim in sub_dim_check:
+				return "当前主维度"+pri_dim+"下，不允许次级维度"+sub_dim
+		return "success"
+	def get_query_sql(self,params):
+		"""根据请求参数，组织查询sql"""
+		dic=json_decode(params)
+		#只有date 和time维度
+		start_date=dic["dateRange"]["start"]
+		end_date=dic["dateRange"]["end"]
+		page=int(dic["page"])
+		pageNum=int(dic["pageNum"])
+		
+		#test sql
+		sql="select reqs_total,impressions_start_total,impressions_finish_total,click,hit_total from ad_facts_by_hour afh limit "+str(pageNum)+" offset "+str((page-1)*pageNum)
+		return sql
+	def get_response(self,rows_result):
+		"""根据数据库的查询结果，组织响应报文"""
+		return "{\"returnCode\":\"200\",\"message\":\"success\"}"
 	def get(self):
 		pass
 	
 	def post(self):
-        #解析body为字典对象
-        #dic=json_decode(params)
         #校验订单报告请求参数
 		params=self.request.body
 		result=self.check_param(params,"order")
-		print result
-# 		result=orderRequestHandler.check_param(ss,"order")
-
-#         print result
-#         if result != "success":
-#             self.write("{\"returnCode\":\"201\",\"message\":\""+result+"\"}")
-#             return
-#         sql=self.get_query_sql(params)
-#         if dic.has_key("dateRange"):
-#             date_range=dic["dateRange"]
-#         orderd=orderDAO()    
-#         rows=orderd.select_all_by_date(date_range)
-#         if rows:
-#             result={"returnCode":"200","message":"success"}
-#             result["result"]={}
-#             for item in rows:
-#                 if not result["result"].has_key(item[0]):
-#                     result["result"][item[0]]={}
-#                 result["result"][item[0]]["msg1"]=item[1]
-#                 result["result"][item[0]]["msg2"]=datetime.datetime.strftime(item[2],'%Y-%m-%d')
-#         str_result=json_encode(result)
-#         self.write(str_result)
-        #取出参数中的维度信息
-#         for i in dic:
-#             print i,'-',dic[i]
-#             if i == "dateRange":
-#                 for x in dic[i]:
-#                     print x,"-",dic[i][x]
-#             if i == "ids":
-#                 for j in dic[i]:
-#                     print j
-#         params=self.get_argument("data")
-#         print params
-
+		if result != "success":
+			self.write("{\"returnCode\":\"201\",\"message\":\""+result+"\"}")
+			pass
+		sql=self.get_query_sql(params)
+		rows=get_resultFromDb(sql)
+		res=self.get_response(rows)
+		self.write(res)
 
 if __name__ == "__main__":
 	application = tornado.web.Application([
