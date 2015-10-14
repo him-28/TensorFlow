179fc391da6ff03d9e19ee1bf69560b6ec9f8d3d
丁拯 <01911@imgo.tv>
2015-10-14 16:26:08 +0800

    添加计算老版本代码

--- etl/etl_job_day.py
+++ etl/etl_job_day.py
@@ -14,15 +14,29 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         is_merge = sys.argv[2] == 'merge'
     is_console_print = False
+    is_old = False
     if len(sys.argv) > 3:
-        is_console_print = sys.argv[3] == 'True'
-    etp = Etl_Transform_Pandas(is_merge,is_console_print)
-    result1 = etp.compute('supply_day_hit', start_time)
-    if result1 == -1:
-        sys.exit(-1)
-    result2 = etp.compute('supply_day_reqs', start_time)
-    if result2 == -1:
-        sys.exit(-1)
-    result3 = etp.compute('demand_day_ad', start_time)
-    if result3 == -1:
-        sys.exit(-1)
+        is_old = sys.argv[3] == 'old'
+    if len(sys.argv) > 4:
+        is_console_print = sys.argv[4] == 'True'
+    etp = Etl_Transform_Pandas(is_merge, is_console_print)
+    if is_old:
+        result1 = etp.compute_old('supply_day_hit', start_time)
+        if result1 == -1:
+            sys.exit(-1)
+        result2 = etp.compute_old('supply_day_reqs', start_time)
+        if result2 == -1:
+            sys.exit(-1)
+        result3 = etp.compute_old('demand_day_ad', start_time)
+        if result3 == -1:
+            sys.exit(-1)
+    else:
+        result1 = etp.compute('supply_day_hit', start_time)
+        if result1 == -1:
+            sys.exit(-1)
+        result2 = etp.compute('supply_day_reqs', start_time)
+        if result2 == -1:
+            sys.exit(-1)
+        result3 = etp.compute('demand_day_ad', start_time)
+        if result3 == -1:
+            sys.exit(-1)
--- etl/etl_job_hour.py
+++ etl/etl_job_hour.py
@@ -10,17 +10,31 @@ import sys
 if __name__ == '__main__':
     date = sys.argv[1]
     hour = "%02d" % int(sys.argv[2])
-    is_console_print = False
+    is_old = False
     if len(sys.argv) > 3:
-        is_console_print = sys.argv[3] == 'True'
+        is_old = sys.argv[3] == 'old'
+    is_console_print = False
+    if len(sys.argv) > 4:
+        is_console_print = sys.argv[4] == 'True'
     date_hour = date + "." + hour
     etp = Etl_Transform_Pandas(False, is_console_print)
-    result1 = etp.compute('supply_hour_hit', date_hour)
-    if result1 == -1:
-        sys.exit(-1)
-    result2 = etp.compute('supply_hour_reqs', date_hour)
-    if result2 == -1:
-        sys.exit(-1)
-    result3 = etp.compute('demand_hour_ad', date_hour)
-    if result3 == -1:
-        sys.exit(-1)
+    if is_old:
+        result1 = etp.compute_old('supply_hour_hit', date_hour)
+        if result1 == -1:
+            sys.exit(-1)
+        result2 = etp.compute_old('supply_hour_reqs', date_hour)
+        if result2 == -1:
+            sys.exit(-1)
+        result3 = etp.compute_old('demand_hour_ad', date_hour)
+        if result3 == -1:
+            sys.exit(-1)
+    else:
+        result1 = etp.compute('supply_hour_hit', date_hour)
+        if result1 == -1:
+            sys.exit(-1)
+        result2 = etp.compute('supply_hour_reqs', date_hour)
+        if result2 == -1:
+            sys.exit(-1)
+        result3 = etp.compute('demand_hour_ad', date_hour)
+        if result3 == -1:
+            sys.exit(-1)
