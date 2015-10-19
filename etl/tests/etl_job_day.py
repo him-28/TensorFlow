d37cd38c676600bf5049402c1aa5838f12db4f40
丁拯 <01911@imgo.tv>
2015-10-19 18:28:30 +0800

    封装方法，方便调用

--- etl/tests/etl_job_day.py
+++ etl/tests/etl_job_day.py
@@ -4,21 +4,11 @@ Created on 2015年10月13日
 
 @author: dico
 '''
-from etl_transform_pandas import Etl_Transform_Pandas
+from etl.logic1.etl_transform_pandas import Etl_Transform_Pandas
 import sys
 
-
-if __name__ == '__main__':
-    start_time = sys.argv[1]
-    is_merge = True
-    if len(sys.argv) > 2:
-        is_merge = sys.argv[2] == 'merge'
-    is_console_print = False
-    is_old = False
-    if len(sys.argv) > 3:
-        is_old = sys.argv[3] == 'old'
-    if len(sys.argv) > 4:
-        is_console_print = sys.argv[4] == 'True'
+def run(start_time,is_merge,is_old,is_console_print):
+    '''run'''
     etp = Etl_Transform_Pandas(is_merge, is_console_print)
     if is_old:
         result1 = etp.compute_old('supply_day_hit', start_time)
@@ -40,3 +30,17 @@ if __name__ == '__main__':
         result3 = etp.compute('demand_day_ad', start_time)
         if result3 == -1:
             sys.exit(-1)
+
+if __name__ == '__main__':
+    START_TIME = sys.argv[1]
+    IS_MERGE = True
+    if len(sys.argv) > 2:
+        IS_MERGE = sys.argv[2] == 'merge'
+    IS_CONSOLE_PRINT = False
+    IS_OLD = False
+    if len(sys.argv) > 3:
+        IS_OLD = sys.argv[3] == 'old'
+    if len(sys.argv) > 4:
+        IS_CONSOLE_PRINT = sys.argv[4] == 'True'
+    run(START_TIME,IS_MERGE,IS_OLD,IS_CONSOLE_PRINT)
+
--- etl/tests/etl_job_hour.py
+++ etl/tests/etl_job_hour.py
@@ -4,19 +4,12 @@ Created on 2015年10月13日
 
 @author: dico
 '''
-from etl_transform_pandas import Etl_Transform_Pandas
+from etl.logic1.etl_transform_pandas import Etl_Transform_Pandas
 import sys
 
-if __name__ == '__main__':
-    date = sys.argv[1]
-    hour = "%02d" % int(sys.argv[2])
-    is_old = False
-    if len(sys.argv) > 3:
-        is_old = sys.argv[3] == 'old'
-    is_console_print = False
-    if len(sys.argv) > 4:
-        is_console_print = sys.argv[4] == 'True'
-    date_hour = date + "." + hour
+def run(the_date,the_hour, is_old, is_console_print):
+    '''run '''
+    date_hour = the_date + "." + the_hour
     etp = Etl_Transform_Pandas(False, is_console_print)
     if is_old:
         result1 = etp.compute_old('supply_hour_hit', date_hour)
@@ -38,3 +31,15 @@ if __name__ == '__main__':
         result3 = etp.compute('demand_hour_ad', date_hour)
         if result3 == -1:
             sys.exit(-1)
+
+if __name__ == '__main__':
+    THE_DATE = sys.argv[1]
+    THE_HOUR = "%02d" % int(sys.argv[2])
+    IS_OLD = False
+    if len(sys.argv) > 3:
+        IS_OLD = sys.argv[3] == 'old'
+    IS_CONSOLE_PRINT = False
+    if len(sys.argv) > 4:
+        IS_CONSOLE_PRINT = sys.argv[4] == 'True'
+    run(THE_DATE, THE_HOUR, IS_OLD, IS_CONSOLE_PRINT)
+    
