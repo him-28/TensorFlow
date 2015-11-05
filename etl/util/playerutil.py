9eb754066c0f2408fe6195a6c3eec2d2e9f40266
jinyibin <jin_yb@imgo.tv>
2015-11-05 18:57:35 +0800

    player 接口

--- etl/util/playerutil.py
+++ etl/util/playerutil.py
@@ -27,22 +27,21 @@ def getplayerInfo():
         ...
         }
     '''
-    d = datetime.datetime.now()
-    ftime=datetime.datetime.strftime(d,"%Y-%m-%d %H:%M")
-    _PLAYERSQL = " select player_id,ad_id,node_id,name,priority,effect_s,effect_e from \"%s\" as s where s.status='1' and s.effect_e >= '%s'  order by player_id,node_id,priority asc" % \
-                (PLAYER_TABLE_NAME,ftime)
+    ed = datetime.datetime.now()
+    endtime=datetime.datetime.strftime(ed,"%Y-%m-%d %H:%M")
+    starttime = getStartTime()
     
-    player_info = {}
+    _getsplittime_sql = " select effect_s from \"%s\" where effect_s <='%s' and effect_e >= '%s' GROUP BY effect_s ORDER by effect_s asc" % \
+                (PLAYER_TABLE_NAME,endtime,starttime,)
+    
     try:
-        rows = DBUtils.fetchall(_PLAYERSQL)
-        if not rows or not len(rows):
-            return {}
-        for row in rows:
-            if not row or len(row) != 7:
-                LOGGER.error("get player info row errors,should be 4 but value: %s" % row)
-                continue
-            _update_player_info(player_info, row)
-        return player_info
+        splitrows = DBUtils.fetchall(_getsplittime_sql)
+        if not splitrows or not len(splitrows):
+            LOGGER.error("can't get player info time split info errors")
+            raise Exception("can't get player info time split info errors")
+        
+        split_player_info = _getSplitInfo(splitrows,ed)
+        return split_player_info
     except Exception,e:
         LOGGER.error("get player info errors, message: %s" % e.message)
     return {}
@@ -96,8 +95,70 @@ def getAllGroupId(dtime=None):
 
 # def getAllGroupIdWithPlayerId():
     
+def getStartTime():    
+    ds = datetime.datetime.now().timetuple()
+    l = time.mktime(ds)
+    l = l-(60*60*24)
+    startDt = datetime.datetime.fromtimestamp(l)
+    starttime = datetime.datetime.strftime(startDt,"%Y-%m-%d %H:%M")
+    return starttime
     
+def _getSplitInfo(rows,endd):
+    index = 0
+    splitInfo = {}
+    for row in rows:
+        if index == 0:
+            index = index + 1
+            starttime = row[0]
+            continue
+        
+        nextendtime = row[0]   
+        splitplayerInfo = _getSplitPlayerInfo(starttime,nextendtime)
+        splitInfo[index] = splitplayerInfo
+        index = index + 1
+        starttime = row[0]
+        
+    splitplayerInfo = _getSplitPlayerInfo(starttime,endd)
+    splitInfo[index] = splitplayerInfo   
+    return splitInfo
+        
+def _getSplitPlayerInfo(starttime,endtime):
+    starttimetuple = starttime.timetuple()
+    endtimetuple = endtime.timetuple()
+    l = time.mktime(starttimetuple)
+    l = l+1
+    startDt = datetime.datetime.fromtimestamp(l)
+    starttimestr = datetime.datetime.strftime(startDt,"%Y-%m-%d %H:%M:%S")  # +1 second    
     
+    l = time.mktime(endtimetuple)
+    l = l-1
+    endDt = datetime.datetime.fromtimestamp(l)
+    endtimestr = datetime.datetime.strftime(endDt,"%Y-%m-%d %H:%M:%S") #  -1 second
+    starttimelong = time.mktime(starttimetuple)
+    endtimelong = time.mktime(endtimetuple)
+    
+    _playersql = " select player_id,ad_id,node_id,name,priority from \"%s\" as s where s.status='1' and \
+             s.effect_s <= '%s' and  s.effect_e >= '%s'  order by player_id,node_id,priority asc" % \
+             (PLAYER_TABLE_NAME,starttimestr,endtimestr)       
+    rows = DBUtils.fetchall(_playersql)
+    player_info = {}
+    try:
+        if not rows or not len(rows):
+            return {}
+        for row in rows:
+            if not row or len(row) != 5:
+                LOGGER.error("get player info row errors,should be 4 but value: %s" % row)
+                continue
+            _update_player_info(player_info, row)
+        splitinfo = {}
+        splitinfo["starttime"] = starttimelong
+        splitinfo["endtime"] = endtimelong
+        splitinfo["playerinfo"] = player_info
+        return splitinfo
+    except Exception,e:
+        LOGGER.error("get player info errors, message: %s" % e.message)
+    return {}
+
 def _update_player_info(playerinfo,row):
     _player=playerinfo.get(row[0])
     
@@ -112,10 +173,3 @@ def _update_player_info(playerinfo,row):
     _slotid.append(row[2])
     _slotid.append(row[3].decode("string_escape").encode('utf8'))
     _slotid.append(row[4])
-    #TODO check
-    ttps = row[5].timetuple()
-    tsstart = time.mktime(ttps)
-    ttpe = row[6].timetuple()
-    tsend = time.mktime(ttpe)
-    _slotid.append(tsstart)
-    _slotid.append(tsend)
