55381a744566c8826f37db0ad054a5709507476e
丁拯 <01911@imgo.tv>
2015-11-13 18:56:52 +0800

    修复列名不一致Bug

--- etl/util/datautil.py
+++ etl/util/datautil.py
@@ -101,6 +101,9 @@ def merge_file(input_paths, output_files):
 
         LOG.info("sum merged datas")
         _group_item = CNF["algorithm"][metric]["group_item"]
+        for _gi in _group_item:
+            if _gi.startswith("query-"):
+                 _group_item[_group_item.index(_gi)] = _gi.replace("query-","")
         df1 = df1.groupby(_group_item).sum()
         df1.to_csv(output_filename, sep=output_column_sep, na_rep=CNF.get("na_rep"), header=True)
 
