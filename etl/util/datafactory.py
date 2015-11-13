776b35b833d160e62ad0b860c61de6c07a3c95d4
丁拯 <01911@imgo.tv>
2015-11-13 15:16:44 +0800

    生成测试数据配置更改

--- etl/util/datafactory.py
+++ etl/util/datafactory.py
@@ -917,7 +917,7 @@ def gen_datetime(min_date=None, max_date=None):
 
 class ADMonitorFactory(object):
     def generate_android_phone(self,out_dir):
-        size = 100000
+        size = 1000
         ips = []
         provinces= []
         citys= []
@@ -971,7 +971,7 @@ class ADMonitorFactory(object):
             slots.append(slot1)
             slots.append(slot2)
             slots.append(slot3)
-            pf_data = gen_choice(['000000', '000100', '000101', '010101'])
+            pf_data = gen_choice(['010101', '010100'])
 
             ips.append(_ip)
             provinces.append(" ")
@@ -1060,7 +1060,7 @@ if __name__ == "__main__":
     import  os
 
     for idx in range(1,25):
-        path = r"F:\data1\ad\2015\11\12\20151112.%s.csv" % idx
+        path = r"F:\data1\ad\2015\11\12\ad_%s.csv" % idx
         if os.path.exists(path):
             os.remove(path)
         gen.generate_android_phone(path)
