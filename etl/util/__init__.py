0ac47a5ad98a0a9659ecb449783e422955337e4a
qinrui <martin.qinrui@gmail.com>
2015-11-16 16:51:24 +0800

    add timer decorator

--- etl/util/__init__.py
+++ etl/util/__init__.py
@@ -1,6 +1,15 @@
 #encoding=utf-8
 
 import os
+import time
 
 def path_chk_or_create(path):
     os.path.exists(path) or os.makedirs(path)
+
+def timer(fn):
+    def wrapper():
+        start = time.clock()
+        fn()
+        end = time.clock()
+        return start, end
+    return wrapper
